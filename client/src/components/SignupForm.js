2d473d6c5755e6bd29aa30a6e952b6094b447583
Jaimit Patel <jaimit_12@hotmail.com>
2022-04-18 18:10:45 -0400

    bug fix

--- client/src/components/SignupForm.js
+++ client/src/components/SignupForm.js
@@ -3,7 +3,6 @@ import { Form, Button, Alert } from 'react-bootstrap';
 import { useMutation } from "@apollo/client";
 import { ADD_USER } from "../utils/mutations";
 import Auth from '../utils/auth';
-import auth from '../utils/auth';
 
 const SignupForm = () => {
   // set initial form state
